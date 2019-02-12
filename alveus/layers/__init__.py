730f06d39dfd3f761cfecc4cc2834d79a11f3845
levifussell <levifussell@hotmail.com>
2019-02-12 20:02:38 +0000

    add all model files to the __init__ for import and likewise with the layers

---
+++ alveus/layers/__init__.py
@@ -0,0 +1,5 @@
+from .LayerReservoir import LayerReservoir
+from .LayerLinearRegression import LayerLinearRegression
+from .LayerEsnReservoir import LayerEsnReservoir
+from .LayerEncoder import LayerPcaEncoder, LayerVaeEncoder
+from .LayerSandpileReservoir import LayerSandpileReservoir
--- alveus/models/__init__.py
+++ alveus/models/__init__.py
@@ -1,1 +1,4 @@
 from .EsnModel import EsnModel
+from .DEsnModel import DEsnModel
+from .DMEsnModel import DMEsnModel
+from .SandPileModel import SandPileModel
